f545f3f7e5239dcdc1b99948caa4c283c531db64
Henrik Lepson <henrikk895@gmail.com>
2023-05-17 21:09:11 +0300

    input box created

--- frontend/src/app/components/pilot/pilot.component.ts
+++ frontend/src/app/components/pilot/pilot.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pilot',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './pilot.component.html',
 })
 export class PilotComponent {
+  loading: boolean = false;
 
+  inputForm: FormGroup = new FormGroup({
+    input: new FormControl(''),
+  });
+
+  async submit(): Promise<void> {
+    const currentText: string | undefined = this.inputForm.get('text')?.value;
+    if (!currentText) return;
+
+    this.loading = true;
+    // TODO: api call
+    this.loading = false;
+  }
+
+  resetForm(): void {
+    this.inputForm.reset({
+      text: '',
+    });
+  }
 }
